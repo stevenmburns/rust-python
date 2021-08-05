7180148c392a9334d5e2dcbd24e765748d8d4fbc
Steven Burns <smburns47@gmail.com>
2021-08-04 23:30:29 -0700

    Only need these

---
+++ doubles.py
@@ -0,0 +1,38 @@
+import re
+import string
+import random
+import myrustlib   #  <-- Import the Rust implemented module (myrustlib.so)
+
+
+def count_doubles(val):
+    """Count repeated pair of chars ins a string"""
+    total = 0
+    for c1, c2 in zip(val, val[1:]):
+        if c1 == c2:
+            total += 1
+    return total
+
+
+double_re = re.compile(r'(?=(.)\1)')
+
+
+def count_doubles_regex(val):
+    return len(double_re.findall(val))
+
+
+val = ''.join(random.choice(string.ascii_letters) for i in range(1000000))
+
+
+def test_pure_python(benchmark):
+    benchmark(count_doubles, val)
+
+
+def test_regex(benchmark):
+    benchmark(count_doubles_regex, val)
+
+
+def test_rust(benchmark):   #  <-- Benchmark the Rust version
+    benchmark(myrustlib.count_doubles, val)
+
+def test_rust_once_bytes(benchmark):   #  <-- Benchmark the Rust version
+    benchmark(myrustlib.count_doubles_once_bytes, val)
---
+++ pyext-myrustlib/src/lib.rs
@@ -0,0 +1,40 @@
+#[macro_use]
+extern crate cpython;
+
+use cpython::{Python, PyResult};
+
+fn count_doubles(_py: Python, val: &str) -> PyResult<u64> {
+    let mut total = 0u64;
+
+    // There is an improved version later on this post
+    for (c1, c2) in val.chars().zip(val.chars().skip(1)) {
+        if c1 == c2 {
+            total += 1;
+        }
+    }
+
+    Ok(total)
+}
+
+fn count_doubles_once_bytes(_py: Python, val: &str) -> PyResult<u64> {
+    let mut total = 0u64;
+
+    let mut chars = val.bytes();
+    if let Some(mut c1) = chars.next() {
+        for c2 in chars {
+            if c1 == c2 {
+                total += 1;
+            }
+            c1 = c2;
+        }
+    }
+
+    Ok(total)
+}
+
+py_module_initializer!(libmyrustlib, initlibmyrustlib, PyInit_myrustlib, |py, m | {
+    m.add(py, "__doc__", "This module is implemented in Rust")?;
+    m.add(py, "count_doubles", py_fn!(py, count_doubles(val: &str)))?;
+    m.add(py, "count_doubles_once_bytes", py_fn!(py, count_doubles_once_bytes(val: &str)))?;
+    Ok(())
+});
